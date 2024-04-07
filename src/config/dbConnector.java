3d39614c205b048c888a56a1bbb85e6c1f2c6ecb
Wangska <wangska1283@gmail.com>
2024-04-07 19:36:53 +0800

    TestLogin

--- src/config/dbConnector.java
+++ src/config/dbConnector.java
@@ -1,12 +1,9 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package config;
 
 import com.mysql.jdbc.Connection;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
@@ -33,5 +30,18 @@ public class dbConnector {
         return rst;
     
     }
+    
+    public boolean insertData(String sql){
+            try{
+                PreparedStatement pst = connect.prepareStatement(sql);
+                pst.executeUpdate();
+                System.out.println("Inserted Successfully!");
+                pst.close();
+               return true;
+            }catch(SQLException ex){
+                System.out.println("Connection Error: "+ex);
+               return false;
+            }
+        }
     
 }
--- src/login/loginform.java
+++ src/login/loginform.java
@@ -1,8 +1,4 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package login;
 
 import adminfolder.admindash;
@@ -11,10 +7,6 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import javax.swing.JOptionPane;
 
-/**
- *
- * @author Sofia
- */
 public class loginform extends javax.swing.JFrame {
 
     
@@ -25,7 +17,7 @@ public class loginform extends javax.swing.JFrame {
     public static boolean loginAcc(String username1, String password1){
         dbConnector connector = new dbConnector();
         try{
-            String query = "SELECT * FROM user WHERE username ='"+username1+"' AND password ='"+password1+"' ";
+            String query = "SELECT * FROM users WHERE username ='"+username1+"' AND password ='"+password1+"' ";
             ResultSet resultSet = connector.getData(query);
             return resultSet.next();
         
@@ -49,10 +41,12 @@ public class loginform extends javax.swing.JFrame {
         jLabel1 = new javax.swing.JLabel();
         jLabel2 = new javax.swing.JLabel();
         jpass = new javax.swing.JTextField();
-        jloginbtn = new javax.swing.JButton();
+        jregisterbutton = new javax.swing.JButton();
         jCheckBox1 = new javax.swing.JCheckBox();
         jButton1 = new javax.swing.JButton();
         juser = new javax.swing.JTextField();
+        jloginbtn1 = new javax.swing.JButton();
+        jLabel5 = new javax.swing.JLabel();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
         setMinimumSize(new java.awt.Dimension(642, 535));
@@ -62,31 +56,31 @@ public class loginform extends javax.swing.JFrame {
         jLabel1.setFont(new java.awt.Font("Arial", 1, 18)); // NOI18N
         jLabel1.setText("Password");
         jPanel1.add(jLabel1);
-        jLabel1.setBounds(130, 230, 110, 30);
+        jLabel1.setBounds(160, 170, 110, 30);
 
         jLabel2.setFont(new java.awt.Font("Arial", 1, 18)); // NOI18N
         jLabel2.setText("Username");
         jPanel1.add(jLabel2);
-        jLabel2.setBounds(130, 170, 120, 30);
+        jLabel2.setBounds(160, 110, 120, 30);
 
         jpass.setFont(new java.awt.Font("Arial", 1, 18)); // NOI18N
         jPanel1.add(jpass);
-        jpass.setBounds(240, 230, 210, 30);
+        jpass.setBounds(270, 170, 210, 30);
 
-        jloginbtn.setFont(new java.awt.Font("Arial", 1, 18)); // NOI18N
-        jloginbtn.setText("Login");
-        jloginbtn.addActionListener(new java.awt.event.ActionListener() {
+        jregisterbutton.setFont(new java.awt.Font("Arial", 1, 18)); // NOI18N
+        jregisterbutton.setText("Register");
+        jregisterbutton.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
-                jloginbtnActionPerformed(evt);
+                jregisterbuttonActionPerformed(evt);
             }
         });
-        jPanel1.add(jloginbtn);
-        jloginbtn.setBounds(410, 300, 120, 40);
+        jPanel1.add(jregisterbutton);
+        jregisterbutton.setBounds(130, 370, 120, 40);
 
         jCheckBox1.setFont(new java.awt.Font("Arial", 1, 18)); // NOI18N
         jCheckBox1.setText("Show Password");
         jPanel1.add(jCheckBox1);
-        jCheckBox1.setBounds(290, 260, 170, 23);
+        jCheckBox1.setBounds(320, 200, 170, 23);
 
         jButton1.setFont(new java.awt.Font("Arial", 1, 18)); // NOI18N
         jButton1.setText("Exit");
@@ -96,11 +90,26 @@ public class loginform extends javax.swing.JFrame {
             }
         });
         jPanel1.add(jButton1);
-        jButton1.setBounds(160, 303, 100, 40);
+        jButton1.setBounds(190, 250, 100, 40);
 
         juser.setFont(new java.awt.Font("Arial", 1, 18)); // NOI18N
         jPanel1.add(juser);
-        juser.setBounds(240, 170, 210, 30);
+        juser.setBounds(270, 110, 210, 30);
+
+        jloginbtn1.setFont(new java.awt.Font("Arial", 1, 18)); // NOI18N
+        jloginbtn1.setText("Login");
+        jloginbtn1.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jloginbtn1ActionPerformed(evt);
+            }
+        });
+        jPanel1.add(jloginbtn1);
+        jloginbtn1.setBounds(420, 250, 120, 40);
+
+        jLabel5.setFont(new java.awt.Font("Tahoma", 1, 18)); // NOI18N
+        jLabel5.setText("Create an Account?");
+        jPanel1.add(jLabel5);
+        jLabel5.setBounds(30, 340, 200, 30);
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
@@ -117,28 +126,39 @@ public class loginform extends javax.swing.JFrame {
         setLocationRelativeTo(null);
     }// </editor-fold>//GEN-END:initComponents
 
-    private void jloginbtnActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jloginbtnActionPerformed
+    private void jregisterbuttonActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jregisterbuttonActionPerformed
         // TODO add your handling code here:
         
-                if(loginAcc(juser.getText(),jpass.getText())){
-                JOptionPane.showMessageDialog(null, "Login Success!");
-                admindash ads = new admindash();
+                
+                Registerform ads = new Registerform();
                 ads.setVisible(true);
                 this.dispose();
-                }else{
-                JOptionPane.showMessageDialog(null, "Login Failed!");
-                
-                }
-        
+      
         
         
                 
         
-    }//GEN-LAST:event_jloginbtnActionPerformed
+    }//GEN-LAST:event_jregisterbuttonActionPerformed
 
     private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
         this.dispose();
     }//GEN-LAST:event_jButton1ActionPerformed
+
+    private void jloginbtn1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jloginbtn1ActionPerformed
+        // login button
+        
+        if(loginAcc(juser.getText(),jpass.getText())){
+           JOptionPane.showMessageDialog(null,"Login Successfully");
+           admindash ads = new admindash();
+           ads.setVisible(true);
+           this.dispose();
+       }else{
+           JOptionPane.showMessageDialog(null, "Invalid User or Password");
+           
+        
+       }
+        
+    }//GEN-LAST:event_jloginbtn1ActionPerformed
 
     /**
      * @param args the command line arguments
@@ -180,9 +200,11 @@ public class loginform extends javax.swing.JFrame {
     private javax.swing.JCheckBox jCheckBox1;
     private javax.swing.JLabel jLabel1;
     private javax.swing.JLabel jLabel2;
+    private javax.swing.JLabel jLabel5;
     private javax.swing.JPanel jPanel1;
-    private javax.swing.JButton jloginbtn;
+    private javax.swing.JButton jloginbtn1;
     private javax.swing.JTextField jpass;
+    private javax.swing.JButton jregisterbutton;
     private javax.swing.JTextField juser;
     // End of variables declaration//GEN-END:variables
 }
